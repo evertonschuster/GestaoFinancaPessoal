819d4faa0868c8d2153150d767086a5a7228b365
everton <everton.schuster98@gmail.com>
2019-03-13 14:18:02 -0300

    Projeto Limpo

---
+++ GestaoFinacaPessoal/GestaoFinacaPessoalOLD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestaoFinacaPessoal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
